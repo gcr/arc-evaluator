e6fc65f615aac22a305484a6c7060e5a6787807e
Michael Wilber <wilmic1102@gmail.com>
2015-05-03 11:20:06 -0400

    revert

--- bbox_utils.py
+++ bbox_utils.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 class BoundingBox(object):
-    def __init__(self, top,left,width,height):
+    def __init__(self, top,left,height,width):
         self.top = top
         self.left = left
-        self.width = width
         self.height = height
+        self.width = width
 
     def __repr__(self):
         return "<Box: %s,%s + %sx%s>"%(self.left, self.top, self.width, self.height)
