bc0293101e2a6fe1e4078dfd57f27308f1adf939
Andreas Veit <accounts@aveit.de>
2015-05-03 19:29:33 -0400

    changed pedestrian to team name

--- PedestrianDataset.py
+++ PedestrianDataset.py
@@ -6,7 +6,7 @@ import bbox_utils
 def load(bounding_box_folder = "Pedestrian/groundTruth"):
     """Load the Caltech Pedestrian Dataset"""
 
-    img_folder = "Pedestrian/images"
+    img_folder = "TeamVision93/images"
     bounding_box_folder = bounding_box_folder
 
     img_file_dict = {}
@@ -15,7 +15,7 @@ def load(bounding_box_folder = "Pedestrian/groundTruth"):
     for file in os.listdir(img_folder):
         img_file_dict[os.path.splitext(file)[0]] = img_folder +"/"+ file
     
-    if bounding_box_folder == "Pedestrian/groundTruth":
+    if bounding_box_folder == "TeamVision93/groundTruth":
         for file in os.listdir(bounding_box_folder):
             bounding_boxes = []
             i = 0
@@ -30,7 +30,7 @@ def load(bounding_box_folder = "Pedestrian/groundTruth"):
                 i += 1
             bbbox_lists_dict[os.path.splitext(file)[0]] = bounding_boxes
 
-    if bounding_box_folder == "Pedestrian/cpu":
+    if bounding_box_folder == "TeamVision93/cpu":
         for file in os.listdir(bounding_box_folder):
             bounding_boxes = []
             filename = bounding_box_folder +"/"+file
