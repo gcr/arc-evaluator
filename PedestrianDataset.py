383e90502b85494522341d01a4eaae8397afc963
Andreas Veit <accounts@aveit.de>
2015-05-03 14:17:32 -0400

    added Pedestrian dataset

---
+++ PedestrianDataset.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import dataset
+import os
+import bbox_utils
+
+def load(bounding_box_folder = "Pedestrian/groundTruth"):
+    """Load the Caltech Pedestrian Dataset"""
+
+    img_folder = "Pedestrian/images"
+    bounding_box_folder = bounding_box_folder
+
+    img_file_dict = {}
+    bbbox_lists_dict = {}
+
+    for file in os.listdir(img_folder):
+        image_filenames.append(file)
+        img_file_dict[os.path.splitext(file)[0]] = file
+    
+    if bounding_box_folder == "Pedestrian/groundTruth":
+        for file in os.listdir(bounding_box_folder):
+            bounding_boxes = []
+            i = 0
+            filename = bounding_box_folder +"/"+file
+            for line in open(filename).readlines():
+                l = line.split()
+                if i == 0:
+                    pass
+                else:
+                    left, top, width, height = [int(l[i]) for i in range(1,5)]
+                    bounding_boxes.append(bbox_utils.BoundingBox(top, left, height, width, confidence = None))
+                i += 1
+            bbbox_lists_dict[os.path.splitext(file)[0]] = bounding_boxes
+
+    if bounding_box_folder == "Pedestrian/cpu":
+        for file in os.listdir(bounding_box_folder):
+            bounding_boxes = []
+            filename = bounding_box_folder +"/"+file
+            for line in open(filename).readlines():
+                l = line.split()
+                left, top, height, width = [int(float(l[i])) for i in range(1,5)]
+                bounding_boxes.append(bbox_utils.BoundingBox(top, left, height, width, confidence = None))
+            bbbox_lists_dict[os.path.splitext(file)[0]] = bounding_boxes
+    return bbbox_lists_dict, img_file_dict
+
+    # records = [
+    #     dataset.DatasetRecord(filename, None, "positive")
+    #     for filename in positive_image_filenames
+    # ] + [
+    #     dataset.DatasetRecord(filename, None, "negative")
+    #     for filename in negative_image_filenames
+    # ]
+
+    # return dataset.Dataset(_name = "UIUC Cars", _folder = folder, _records = records)
